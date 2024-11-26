f05e285370b2ed3922405dd10628d3bc74c1c11d
vinuth <vinuthsriarampath@outlook.com>
2024-11-26 13:36:23 +0530

    change appbar colour and fontsize and icon sizes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,14 +10,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.menu),
-          title: Text("Easy Flutter App"),
-          backgroundColor:Colors.blue,
-          actions: [
-            Icon(Icons.search)
+          leading: const Icon(
+            Icons.menu ,
+            size: 30,
+            color: Colors.white,
+          ),
+          title: const Text("Easy Flutter App", style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.w400,
+          ),),
+          backgroundColor:const Color(0xff0E00AC),
+          actions:const [
+            Icon(
+              Icons.search,
+              size: 30,
+              color: Colors.white,
+            )
           ],
+        ),
+        body:const Center(
+            child: Text("Hello friends , my name is flutter",style: TextStyle(
+              fontSize: 20
+            ),)
         ),
       ),
     );
